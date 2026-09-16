f229b840e888ef456e1d1e3cfe99802069f0210a
Ramifus <ramifus926@gmail.com>
2026-09-16 11:40:32 -0400

    asdfas

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  // hace falta para que las etiquetas og: salgan con URL absoluta
+  site: "https://baby-shower-nicolas-andre-2026.vercel.app",
   vite: {
     plugins: [tailwindcss()]
   }
